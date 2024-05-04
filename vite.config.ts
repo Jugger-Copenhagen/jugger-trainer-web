4bdc2c3bd57a4d5116857e41720d4266b02bebfb
Evan Savage <evan.savage@lego.com>
2024-05-04 18:50:56 +0200

    Set different base URL in CI
    so that we can deploy this on Github Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
+const isCi = !!process.env.CI;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isCi ? '/jugger-trainer-web' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
