765dfcd5b106bc426285e430c496fa3919be2c4d
Evan Savage <evan.savage@lego.com>
2024-05-04 19:39:29 +0200

    Switch to production database
    Now that we're using the realtime database, it works in both places.  No idea why it completely changed the database format when it was copied the first time.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,14 @@ import { DocumentData } from 'firebase/firestore';
 import { getDownloadURL, getStorage, listAll, ref } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDyVMCRC9Toba_G8dp5aweN9Z7Ap479sRw',
-  authDomain: 'jugger-trainer-web.firebaseapp.com',
-  databaseURL: 'https://jugger-trainer-web-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'jugger-trainer-web',
-  storageBucket: 'jugger-trainer-web.appspot.com',
-  messagingSenderId: '987757334310',
-  appId: '1:987757334310:web:b45a9e20e317ca03e6fbe7',
+  apiKey: 'AIzaSyBZJmUnbSqufRL64CSzdJMpy3hlRshd-9Q',
+  authDomain: 'juggertrainer.firebaseapp.com',
+  databaseURL: 'https://juggertrainer.firebaseio.com',
+  projectId: 'juggertrainer',
+  storageBucket: 'juggertrainer.appspot.com',
+  messagingSenderId: '103859061629',
+  appId: '1:103859061629:web:eff53ba99a8251f65cc214',
+  measurementId: 'G-KN7W5GRDM4',
 };
 
 const app = initializeApp(firebaseConfig);
