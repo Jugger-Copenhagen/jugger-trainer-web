06011a6aadbb563999b561cb2c8356278d7a923b
Evan Savage <evan.savage@lego.com>
2025-03-30 15:42:21 +0200

    auth with email / password working

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,29 +1,76 @@
 import { auth } from '@/lib/firebase';
 import { useUserStore } from '@/lib/store';
-import { Google } from '@mui/icons-material';
-import { Box, Button, Grid } from '@mui/material';
+import { AppRegistration, Google, Login as IconLogin } from '@mui/icons-material';
+import { Box, Button, Divider, Grid, TextField } from '@mui/material';
 import { FirebaseError } from 'firebase/app';
 import {
   GoogleAuthProvider,
   OAuthCredential,
+  createUserWithEmailAndPassword,
   linkWithCredential,
+  signInWithEmailAndPassword,
   signInWithPopup,
 } from 'firebase/auth';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Form, useNavigate } from 'react-router-dom';
 
 let pendingCredential: OAuthCredential | null = null;
 
 export default function Login() {
-  // const actionData = useActionData() as FormErrors | null;
   const userStore = useUserStore();
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     if (userStore.user !== null) {
       navigate('/');
     }
   }, [userStore.user]);
+
+  function handleAuthError(err: unknown) {
+    if (!(err instanceof FirebaseError)) {
+      throw err;
+    }
+
+    const { code, message } = err;
+    console.error(code, message);
+  }
+
+  async function signInWithAppUser() {
+    // TODO: validate these
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      let user = result.user;
+      if (pendingCredential !== null) {
+        const credentialLinked = await linkWithCredential(user, pendingCredential);
+        user = credentialLinked.user;
+      }
+
+      userStore.setUser(user);
+    } catch (err) {
+      handleAuthError(err);
+    }
+  }
+
+  async function createAppUser() {
+    // TODO: validate these
+    try {
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+
+      let user = result.user;
+      if (pendingCredential !== null) {
+        const credentialLinked = await linkWithCredential(user, pendingCredential);
+        user = credentialLinked.user;
+      }
+
+      userStore.setUser(user);
+    } catch (err) {
+      handleAuthError(err);
+    }
+  }
 
   async function signInWithGoogle() {
     auth.useDeviceLanguage();
@@ -41,17 +88,7 @@ export default function Login() {
 
       userStore.setUser(user);
     } catch (err) {
-      if (!(err instanceof FirebaseError)) {
-        throw err;
-      }
-
-      if (err.code === 'auth/account-exists-with-different-credential') {
-        pendingCredential = GoogleAuthProvider.credentialFromError(err);
-
-        // TODO: prompt the user to sign in with the same provider they used to sign up
-      }
-
-      // TODO: handle this better
+      handleAuthError(err);
     }
   }
 
@@ -60,44 +97,56 @@ export default function Login() {
       <Grid item sm={12} md={8} lg={6}>
         <h2>Login</h2>
 
-        {/* <Form method="post">
+        <Form method="post">
           <TextField
+            type="email"
             name="email"
             label="Email"
+            value={email}
             variant="outlined"
             fullWidth
-            error={!!actionData?.fieldErrors?.email}
-            helperText={actionData?.fieldErrors?.email}
+            onChange={(evt) => setEmail(evt.target.value)}
           />
           <TextField
             type="password"
             name="password"
             label="Password"
+            value={password}
             variant="outlined"
             fullWidth
-            error={!!actionData?.fieldErrors?.password}
-            helperText={actionData?.fieldErrors?.password}
             sx={{ mt: 2 }}
+            onChange={(evt) => setPassword(evt.target.value)}
           />
 
-          <Button type="submit" variant="contained" color="primary" size="large" sx={{ mt: 2 }}>
-            Submit
+          <Button
+            type="button"
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{ mt: 2 }}
+            onClick={signInWithAppUser}
+          >
+            Sign In <IconLogin sx={{ ml: 1 }} />
+          </Button>
+
+          <Button
+            type="button"
+            variant="outlined"
+            size="large"
+            sx={{ ml: 2, mt: 2 }}
+            onClick={createAppUser}
+          >
+            Sign Up <AppRegistration sx={{ ml: 1 }} />
           </Button>
         </Form>
 
-        <Divider sx={{ my: 4 }} /> */}
-
-        <p>Register + sign-in with username and password coming soon.</p>
+        <Divider sx={{ my: 4 }} />
 
         <Box sx={{ mb: 4 }}>
           <Button size="large" variant="outlined" onClick={signInWithGoogle}>
             <Google sx={{ mr: 1 }} /> Sign in with Google
           </Button>
         </Box>
-
-        {/* <p>
-          No account yet? <Link to="/register">Register here.</Link>
-        </p>--> */}
       </Grid>
     </Grid>
   );
