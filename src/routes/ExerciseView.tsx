cf0b583d66c5f6f3852bee8366a004b0221bb564
Evan Savage <savage.evan@pm.me>
2025-05-29 17:39:36 +0200

    add edit icon fab to nowhere

--- src/routes/ExerciseView.tsx
+++ src/routes/ExerciseView.tsx
@@ -1,9 +1,28 @@
 import ExerciseDetails from '@/components/ExerciseDetails';
+import { useUserStore } from '@/lib/store';
 import { loaderExerciseView } from '@/routes/loaders';
-import { useLoaderData } from 'react-router-dom';
+import { Edit } from '@mui/icons-material';
+import { Box, Fab } from '@mui/material';
+import { Link, useLoaderData } from 'react-router-dom';
 
 export default function ExerciseView() {
+  const { user } = useUserStore();
   const exercise = useLoaderData() as Awaited<ReturnType<typeof loaderExerciseView>>;
 
-  return <ExerciseDetails exercise={exercise} />;
+  return (
+    <Box>
+      <ExerciseDetails exercise={exercise} />
+      {user && (
+        <Fab
+          aria-label={`Edit exercise: ${exercise.name}`}
+          color="secondary"
+          component={Link}
+          sx={{ position: 'fixed', bottom: 16, right: 16 }}
+          to={`/exercises/edit/${exercise.eid}`}
+        >
+          <Edit />
+        </Fab>
+      )}
+    </Box>
+  );
 }
