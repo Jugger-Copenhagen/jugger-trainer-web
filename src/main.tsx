96834fe2dd7604c801e9ae793354c21ba46c3b9a
Evan Savage <savage.evan@pm.me>
2026-03-01 16:42:27 +0100

    chore: v7 future flags all enabled

--- src/main.tsx
+++ src/main.tsx
@@ -60,11 +60,18 @@ const router = createBrowserRouter(
   ],
   {
     basename: '/jugger-trainer-web',
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
   }
 );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
   </React.StrictMode>
 );
