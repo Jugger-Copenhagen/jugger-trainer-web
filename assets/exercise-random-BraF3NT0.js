4064ad63bbf31f73c01255505d1fc324d84f296c
candu <candu@users.noreply.github.com>
2026-03-06 15:55:04 +0000

    deploy: a41410a6aa8c05eaab3bd70be5377a9344efb320

---
+++ assets/exercise-random-BraF3NT0.js
@@ -0,0 +1,2 @@
+import{w as e,o}from"./chunk-LFPYN7LY-DFwpsnzj.js";const s=e(function(){return o.jsx("div",{children:"Random exercise"})});export{s as default};
+//# sourceMappingURL=exercise-random-BraF3NT0.js.map
--- assets/exercise-random-Ce5IXmXB.js
+++
@@ -1,2 +0,0 @@
-import{w as e,o}from"./chunk-LFPYN7LY-42L3dXPP.js";const s=e(function(){return o.jsx("div",{children:"Random exercise"})});export{s as default};
-//# sourceMappingURL=exercise-random-Ce5IXmXB.js.map
